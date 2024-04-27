3c1e8118dc87825a69d3779ad595c44d668176b2
Shepard Jiang <shepardjiang2025@gmail.com>
2024-04-27 14:16:28 -0400

    Implement estimated CO2 savings + csp polling

--- distance.py
+++ distance.py
@@ -1,9 +1,15 @@
 import os
 import googlemaps
+from csp_bike import get_stations_df
+import pandas as pd
+import math
+from functools import lru_cache
+import datetime
 
 maps_client = googlemaps.Client(key=os.environ['GOOGLE_MAPS_API_KEY'])
 
 # Get biking distance between two locations
+@lru_cache
 def get_distance_by_mode(ori_lat, ori_lng, dest_lat, dest_lng, mode):
     if mode == 'b':
         mode = 'bicycling'
@@ -17,6 +23,7 @@ def get_distance_by_mode(ori_lat, ori_lng, dest_lat, dest_lng, mode):
     )
     return distance['rows'][0]['elements'][0]['distance']['text']
 
+@lru_cache
 def get_travel_time_by_mode(ori_lat, ori_lng, dest_lat, dest_lng, mode):
     if mode == 'b':
         mode = 'bicycling'
@@ -30,6 +37,7 @@ def get_travel_time_by_mode(ori_lat, ori_lng, dest_lat, dest_lng, mode):
     )
     return distance['rows'][0]['elements'][0]['duration']['text']
 
+@lru_cache
 def estimate_co2_saved(ori_lat, ori_lng, dest_lat, dest_lng):
     bike_distance = get_distance_by_mode(ori_lat, ori_lng, dest_lat, dest_lng, 'b')
     driving_distance = get_distance_by_mode(ori_lat, ori_lng, dest_lat, dest_lng, 'd')
@@ -55,6 +63,7 @@ def estimate_co2_saved(ori_lat, ori_lng, dest_lat, dest_lng):
 
     return driving_emissions - bike_emissions
 
+@lru_cache
 def estimate_delta_time(ori_lat, ori_lng, dest_lat, dest_lng):
     est_bike_time = get_travel_time_by_mode(ori_lat, ori_lng, dest_lat, dest_lng, 'b')
     driving_time = get_travel_time_by_mode(ori_lat, ori_lng, dest_lat, dest_lng, 'd')
@@ -77,3 +86,60 @@ def estimate_delta_time(ori_lat, ori_lng, dest_lat, dest_lng):
 
 # print(estimate_co2_saved(40.746153593,-73.916188598,40.67308,-73.94191))
 # print(estimate_delta_time(40.746153593,-73.916188598,40.67308,-73.94191))
+
+@lru_cache
+def estimate_lat_lng_to_km(ori_lat, ori_lng, dest_lat, dest_lng):
+    # units in km
+    radius = 6371
+
+    dlat = math.radians(dest_lat - ori_lat)
+    dlon = math.radians(dest_lng - ori_lng)
+
+    a = (math.sin(dlat / 2) * math.sin(dlat / 2) +
+         math.cos(math.radians(ori_lat)) * math.cos(math.radians(dest_lat)) *
+         math.sin(dlon / 2) * math.sin(dlon / 2))
+    
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    return radius * c
+
+def is_trip_possible(ori_lat, ori_lng, dest_lat, dest_lng, start_time: datetime.datetime, end_time: datetime.datetime):
+    est_distance = estimate_lat_lng_to_km(ori_lat, ori_lng, dest_lat, dest_lng)
+    time_elapsed = (end_time - start_time).total_seconds() / 3600
+
+    # fastest reasonable biking speed in the city is 20 km/h
+    return est_distance / 20 <= time_elapsed
+
+# def compute_station_matrix():
+#     # pd.read_csv('distance_matrix.csv', delimiter=',')
+
+#     station_df = get_stations_df()
+
+#     station_matrix = {}
+#     for _, row in station_df.iterrows():
+#         station_matrix[row['station_id']] = (row['lat'], row['lon'])
+#     # return station_matrix
+        
+#     distance_matrix = pd.DataFrame()
+#     data = {}
+#     indices = []
+#     for (ori_id, (lat, lon)) in station_matrix.items():
+#         for (dest_id, (dest_lat, dest_lng)) in station_matrix.items():
+#             if dest_id not in data:
+#                 data[dest_id] = []
+#             distance = 1.5 * get_lat_lng_to_km(lat, lon, dest_lat, dest_lng)
+
+#             # distance_matrix.at[ori_id, dest_id] = distance
+#             data[dest_id].append(distance)
+        
+#         indices.append(ori_id)
+#         # break
+    
+#     distance_matrix = pd.Dataframe(data, index=indices)
+            
+#     distance_matrix.to_csv('distance_matrix.csv', sep=',', index=False)
+
+
+if __name__ == '__main__':
+    pass
+
--- poll.py
+++ poll.py
@@ -1,7 +1,10 @@
 import csp
 from csp import ts
 from datetime import timedelta
-from csp_bike import get_station_status
+from csp_bike import get_station_status, get_stations
+import distance
+import datetime
+import random
 
 @csp.node
 def poll_data(interval: timedelta) -> ts[[dict]]:
@@ -46,12 +49,88 @@ def calculate_total_system_capacity(stations: ts[[dict]]) -> ts[int]:
         # finally, "tick" out the result
         return s_capacity
     
+@csp.node
+def approximate_trips(stations: ts[[dict]]) -> ts[float]:
+    with csp.state():
+        # these are stateful variables that will retain their
+        # value in between "ticks"
+        station_data = get_stations()
+        # print(station_data)
+
+        # bikes currently in service, stored with time checked out and stationID
+        s_bike_pool = set()
+
+        # tabulate total CO2 saved
+        s_co2_saved = 0
+
+        # stations and their previous capacities, stored to track number of bikes checked out
+        s_stations = {}
+
+        init = True
+
+    if csp.ticked(stations):
+        # when a new list of stations "ticks", we'll do some
+        # processing and emit a new "tick" as output
+
+        for station in stations:
+            # subtract prior capacity
+            prior_capacity = s_stations.get(station["station_id"], {}).get("total_bikes_available", 0)
+            current_capacity = station["total_bikes_available"]
+            current_lat = station_data[station["station_id"]]["lat"]
+            current_lon = station_data[station["station_id"]]["lon"]
+
+            # if the station has less bikes than before, we assume they were checked out
+            if current_capacity < prior_capacity:
+                bikes_checked_out = prior_capacity - current_capacity
+
+                s_bike_pool.add((station["station_id"], current_lat, current_lon, datetime.datetime.now() , bikes_checked_out))
+            
+            elif current_capacity > prior_capacity:
+                current_time = datetime.datetime.now()
+
+                bikes_returned = current_capacity - prior_capacity
+            
+                # bikes part of a possible trip
+                potential_bikes = [bike_data for bike_data in s_bike_pool if 
+                                     distance.is_trip_possible(bike_data[1], bike_data[2], current_lat, current_lon, bike_data[3], current_time)]
+                
+
+
+                for _ in range(bikes_returned):
+                    if len(potential_bikes) == 0:
+                        # average trip length: 12 minutes, 1.75 miles = 2.8 km
+                        # car emissions - bike emissions
+                        s_co2_saved += 2.8 * 0.192 - 2.8 * 0.021
+                        continue
+
+                    # otherwise randomly choose a possible bike for the trip
+                    random_bike = random.choice(potential_bikes)
+                    s_bike_pool.discard(random_bike)
+                    if random_bike[4] > 1:
+                        s_bike_pool.add((random_bike[0], random_bike[1], random_bike[2], current_time, random_bike[4] - 1))
+                    
+                    s_co2_saved += distance.estimate_co2_saved(random_bike[1], random_bike[2], current_lat, current_lon)
+
+            # now update our tracker and capacity
+            s_stations[station["station_id"]] = station
+
+        if init:
+            init = False
+            s_co2_saved = 0
+
+        print("bike pool: ", sum([bike[4] for bike in s_bike_pool]))
+    
+        # finally, "tick" out the result
+        return s_co2_saved
+    
 @csp.graph
 def my_capacity_calculator(interval: timedelta):
     stations_data = poll_data(interval=interval)
-    system_capacity = calculate_total_system_capacity(stations_data)
-    csp.print("Total system capacity", system_capacity)
+    # system_capacity = calculate_total_system_capacity(stations_data)
+    # csp.print("Total system capacity", system_capacity)
+    co2_saved = approximate_trips(stations_data)
+    csp.print("Total CO2 saved", co2_saved)
 
-csp.run(my_capacity_calculator, timedelta(seconds=5), realtime=True)
+csp.run(my_capacity_calculator, timedelta(seconds=15), realtime=True)
 
         
